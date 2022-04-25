3b0408a221fc9e19ca9aee1ac316f2c80a4923af
sssssqew <sssssqew@gmail.com>
2022-04-25 13:52:28 +0900

    서버 첫 구동

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-console.log('hello world')
+var express = require('express')
+var app = express() // app : 객체
 
-var a = 3
+// 사용자가 localhost:5000/hello URL 주소로 접근했을때
+app.get('/hello', (req, res) => {
+    res.send('hello world !') // 응답을 브라우저로 보내줌
+})
+
+// 맨 밑에 놓기
+app.listen(5000, () => {
+    console.log('server is runnin port 5000 !')
+})
